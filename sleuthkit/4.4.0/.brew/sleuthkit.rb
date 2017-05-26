3bd90cf4df06443010c914bd157134c93974a8e1
Ralic Lo <raliclo@Ralics-MacBook-Pro.local>
2017-05-26 03:01:23 -0700

    Update more prebuilt libs

---
+++ sleuthkit/4.4.0/.brew/sleuthkit.rb
@@ -0,0 +1,44 @@
+class Sleuthkit < Formula
+  desc "Forensic toolkit"
+  homepage "https://www.sleuthkit.org/"
+  url "https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.4.0/sleuthkit-4.4.0.tar.gz"
+  sha256 "7d252562622f657001e080777c5fe1fd919b952fa3d658c86a62e57b6ad70f57"
+
+  conflicts_with "irods", :because => "both install `ils`"
+
+  option "with-jni", "Build Sleuthkit with JNI bindings"
+  option "with-debug", "Build debug version"
+
+  if build.with? "jni"
+    depends_on :java
+    depends_on :ant => :build
+  end
+
+  depends_on "afflib" => :optional
+  depends_on "libewf" => :optional
+
+  conflicts_with "ffind",
+    :because => "both install a 'ffind' executable."
+
+  def install
+    ENV.append_to_cflags "-DNDEBUG" if build.without? "debug"
+    ENV.java_cache if build.with? "jni"
+
+    system "./configure", "--disable-dependency-tracking",
+                          if build.without? "jni" then "--disable-java" end,
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+
+    if build.with? "jni"
+      cd "bindings/java" do
+        system "ant"
+      end
+      prefix.install "bindings"
+    end
+  end
+
+  test do
+    system "#{bin}/tsk_loaddb", "-V"
+  end
+end
