fdb21d1996e85889b0e17716538ceb0d1218ebfd
ralic <raliclo@gmail.com>
2017-07-20 20:21:09 -0700

    Update more

---
+++ heroku/6.12.11/.brew/heroku.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class Heroku < Formula
+  desc "Command-line client for the cloud PaaS"
+  homepage "https://cli.heroku.com"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.12.11.tgz"
+  sha256 "5420fae717c175de3acba2f1e444af799a2e587efe16e4aab7c351ce6264bbc3"
+  head "https://github.com/heroku/cli.git"
+
+  depends_on "node"
+
+  def install
+    inreplace "bin/run.js", "npm update -g heroku-cli", "brew upgrade heroku"
+    inreplace "bin/run", "node \"$DIR/run.js\"",
+                         "#{Formula["node"].opt_bin}/node \"$DIR/run.js\""
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
---
+++ libev/4.24/.brew/libev.rb
@@ -0,0 +1,16 @@
+class Libev < Formula
+  desc "Asynchronous event library"
+  homepage "http://software.schmorp.de/pkg/libev.html"
+  url "http://dist.schmorp.de/libev/Attic/libev-4.24.tar.gz"
+  sha256 "973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+
+    # Remove compatibility header to prevent conflict with libevent
+    (include/"event.h").unlink
+  end
+end
