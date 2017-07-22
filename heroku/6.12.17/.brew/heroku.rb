5047850f154a3db856bd71ebb3cc46f3923c711e
ralic <raliclo@gmail.com>
2017-07-21 18:31:31 -0700

    Upgrade llvm, heroku, mercurial

---
+++ heroku/6.12.17/.brew/heroku.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class Heroku < Formula
+  desc "Command-line client for the cloud PaaS"
+  homepage "https://cli.heroku.com"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.12.17.tgz"
+  sha256 "1a0e014de6355d90ecac2b7a95394160f08245b9630bb92305f3444bf6b6446c"
+  head "https://github.com/heroku/cli.git"
+
+  depends_on "node"
+
+  def install
+    inreplace "bin/run.js", "npm update -g heroku-cli", "brew upgrade heroku"
+    inreplace "bin/run", "node \"$DIR/run.js\"",
+                         "#{Formula["node"].opt_bin}/node \"$DIR/run.js\""
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
