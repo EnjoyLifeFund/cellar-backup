041114eba42323d57dde8ad8584a564308bd1b30
ralic <raliclo@gmail.com>
2017-09-02 22:58:05 -0700

    Update packages

---
+++ heroku/6.14.13/.brew/heroku.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class Heroku < Formula
+  desc "Command-line client for the cloud PaaS"
+  homepage "https://cli.heroku.com"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.14.13.tgz"
+  sha256 "108f251179853947b48ced7b6a6aa2df07c26d5d4b9d1cad93e4f99a53f0e885"
+  head "https://github.com/heroku/cli.git"
+
+  depends_on "node"
+
+  def install
+    inreplace "bin/run.js", "npm update -g heroku-cli", "brew upgrade heroku"
+    inreplace "bin/run", "node \"$DIR/run.js\"",
+                         "#{Formula["node"].opt_bin}/node \"$DIR/run.js\""
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
---
+++ lzo/2.10/.brew/lzo.rb
@@ -0,0 +1,15 @@
+class Lzo < Formula
+  desc "Real-time data compression library"
+  homepage "https://www.oberhumer.com/opensource/lzo/"
+  url "https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
+  sha256 "c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-shared"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+end
