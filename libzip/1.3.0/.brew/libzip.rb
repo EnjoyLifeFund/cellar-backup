69fa22f8a61a3a02ac59eab73205f55ddba9873c
ralic <raliclo@gmail.com>
2017-09-03 16:29:11 -0700

    Update library

---
+++ libzip/1.3.0/.brew/libzip.rb
@@ -0,0 +1,24 @@
+class Libzip < Formula
+  desc "C library for reading, creating, and modifying zip archives"
+  homepage "https://www.nih.at/libzip/"
+  url "https://www.nih.at/libzip/libzip-1.3.0.tar.xz"
+  sha256 "aa936efe34911be7acac2ab07fb5c8efa53ed9bb4d44ad1fe8bff19630e0d373"
+
+  conflicts_with "libtcod", :because => "both install `zip.h` header"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "CXX=#{ENV.cxx}",
+                          "CXXFLAGS=#{ENV.cflags}"
+    system "make", "install"
+  end
+
+  test do
+    touch "file1"
+    system "zip", "file1.zip", "file1"
+    touch "file2"
+    system "zip", "file2.zip", "file1", "file2"
+    assert_match /\+.*file2/, shell_output("#{bin}/zipcmp -v file1.zip file2.zip", 1)
+  end
+end
